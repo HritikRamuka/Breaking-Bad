677de2540759ce7d3442413368972f31eb55f7d8
unknown <ramukahritik24@gmail.com>
2021-08-15 12:15:47 +0530

    third commit

--- src/Screens/Details.jsx
+++ src/Screens/Details.jsx
@@ -48,12 +48,12 @@ console.log(mapped);
                <img src={details[0].img} alt="" />
                <div className="info">
                <h3> Name: {details[0].name}</h3>
-               <h4>DOB: {details[0].birthday}</h4>
+               <h5>DOB: {details[0].birthday}</h5>
                <h5>Occupation: {occupations}</h5>
                <h5>Nickname: {details[0].nickname}</h5>
                <h5>Status: {details[0].status}</h5>
                <h5>Appearance: {mapped}</h5>
-               <h3 className="quotes"><i class="fas fa-quote-left"></i> {quotes[0].quote} <i class="fas fa-quote-right"></i>   </h3>
+               <p className="quotes"><i class="fas fa-quote-left"></i> {quotes[0].quote} <i class="fas fa-quote-right"></i>   </p>
                </div>
            </div>
            
